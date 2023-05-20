e9655319e5697b2a81f5d9aa052a9c7f1ed0e598
kajj8808 <a01039388808@gmail.com>
2023-05-21 03:05:41 +0900

    student 1 기능 추가

--- pages/student1.tsx
+++ pages/student1.tsx
@@ -11,13 +11,33 @@ export default function Student1() {
   const { register, handleSubmit, watch } = useForm<IForm>();
   const [picrurePreview, setPicrurePreview] = useState("");
   const [uploadError, setUploadError] = useState<String | null>(null);
+  const [stuNum, setStunum] = useState("");
+  const [isUploaded, setIsUploaded] = useState(false);
 
   const onValid = ({ picrure }: IForm) => {
     if (picrure) {
       const fileReader = new FileReader();
       fileReader.readAsDataURL(picrure[0]);
       fileReader.onload = () => {
-        console.log(fileReader.result);
+        const base64 = fileReader.result
+          ?.toString()
+          .split("data:image/jpeg;base64,")[1];
+        fetch("https://facecheck.run-asia-northeast1.goorm.site/register", {
+          method: "POST",
+          mode: "cors",
+          headers: { "Content-type": "application/json;charset=utf-8" },
+          body: JSON.stringify({
+            encodedImage: base64,
+            studentID: stuNum + "",
+          }),
+        }).then(async (res) => {
+          if (res.status == 200) {
+            const { msg } = await res.json();
+            if (msg === "얼굴이 정상적으로 등록되었습니다") {
+              setIsUploaded(true);
+            }
+          }
+        });
       };
     }
   };
@@ -38,10 +58,10 @@ export default function Student1() {
   //#1d1d1f
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-[#fbfbfd] px-20">
-      <div className="rounded-2xl bg-white shadow-2xl">
-        <form onSubmit={handleSubmit(onValid)}>
-          <div className="flex w-[80vw] flex-col items-center py-5 md:w-[380px]">
-            <div className="relative h-28 w-28 overflow-hidden rounded-full bg-[rgba(0,0,0,0.2)] shadow-md ring-1 ring-stone-50">
+      <form onSubmit={handleSubmit(onValid)}>
+        <div className="rounded-2xl bg-white shadow-2xl">
+          <div className="flex w-[90vw] flex-col items-center py-10 md:w-[380px]">
+            <div className="relative h-24 w-24 overflow-hidden rounded-full bg-[rgba(0,0,0,0.1)] shadow-md ">
               <input
                 type="file"
                 {...register("picrure")}
@@ -57,9 +77,9 @@ export default function Student1() {
                 />
               ) : null}
             </div>
-            <div className="mt-4 flex flex-col">
+            <div className="mt-5 flex flex-col">
               <div className="flex flex-col items-center">
-                <span className="mb-1.5 text-xl font-medium">5671144</span>
+                <span className="mb-1.5 text-xl font-medium ">5671144</span>
                 <span className="text-xs text-gray-500">Description</span>
               </div>
 
@@ -76,13 +96,20 @@ export default function Student1() {
               </div>
             </div>
           </div>
-        </form>
-      </div>
-      <div className="">
-        <div className="mt-24 w-32 rounded-xl bg-blue-500 p-3 text-center text-white hover:bg-teal-500 focus:bg-red-500 active:bg-yellow-500">
-          <span>Checkout</span>
         </div>
-      </div>
+        <div className="mt-16  flex w-full justify-center">
+          <button
+            disabled={isUploaded}
+            className={` w-32 rounded-xl ${
+              isUploaded
+                ? "bg-slate-500 text-gray-400"
+                : "bg-blue-500 hover:bg-teal-500 focus:bg-red-500 active:bg-yellow-500"
+            } p-3 text-center text-white `}
+          >
+            <span>Checkout</span>
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
